303dca08449f752beafe423c8a928dfd379316ad
filalaa <169075625+filalaa@users.noreply.github.com>
2024-05-08 05:16:00 +0300

    Update game.js

--- game.js
+++ game.js
@@ -1,7 +1,7 @@
 const canvas = document.getElementById('game');
 const context = canvas.getContext('2d');
 const playerImg = new Image();
-playerImg.src = 'data/gfx/player.png';
+playerImg.src = 'gfx/player.png';
 
 // width and height of each platform and where platforms start
 const platformWidth = 65;
